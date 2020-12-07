66aa9736592e6400e9f7b96c29188254808b504b
Dennis <secreth92@gmail.com>
2020-12-07 12:13:01 +0100

    First commitment

---
+++ Library/Library/Library.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    class Library
+    {
+
+        public string LibraryName { get; }
+
+        public string GetLibrary()
+        {
+            return $"Welcome to {LibraryName} the date today is: {DateTime.Now.ToShortDateString()}";
+        }
+
+        public Library(string name)
+        {
+            LibraryName = name;
+        }
+    }
+}
---
+++ Library/Library/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library lib = new Library("fml lib");
+
+            Console.WriteLine(lib.GetLibrary());
+        }
+    }
+}
