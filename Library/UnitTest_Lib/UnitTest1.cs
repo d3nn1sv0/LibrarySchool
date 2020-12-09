3d028ae832da701e30b414c51d10f10dfabec4c5
Dennis <secreth92@gmail.com>
2020-12-09 10:36:39 +0100

    Unit tested

--- Library/UnitTest_Lib/UnitTest1.cs
+++ Library/UnitTest_Lib/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
 using Xunit;
+using Library;
 
 namespace UnitTest_Lib
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void GetLoaneeTest()
         {
 
+            Library.Library testLib = new Library.Library("Test Lib");
+
+            testLib.LoanerList.Add(new Loanee(1, "Johnny", "johnny@lux.dk"));
+
+            string result = testLib.GetLoanee(1);
+
+            Assert.Contains("Johnny", result);
         }
     }
 }
